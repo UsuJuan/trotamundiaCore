ffb4896f0faef1f97cd0f02e814397b12a79a358
Juan Ortiz <juan.ortiz.oviedo@gmail.com>
2017-03-06 03:17:21 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+# import os
+from setuptools import setup, find_packages
+
+# README = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+# REQUIRES = [i.strip() for i in open("requirements.txt").readlines()]
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+__version__ = '0.1.4'
+__description__ = 'Liberia de Trotamundia para Django',
+__license__ = 'BSD'
+__author__ = 'Juan Ortiz',
+__email__ = 'juan.ortiz@trotamundia.com',
+
+
+setup(
+    name='trotamundia',
+    version=__version__,
+    packages=find_packages(),
+    include_package_data=True,
+    license=__license__,
+    description=__description__,
+    url='http://www.trotamundia.com/',
+    author=__author__,
+    author_email=__email__,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=['django'],
+    # dependency_links=[
+    #     'git+https://gianburga@bitbucket.org/gianburga/django-oauth-toolkit-mongoengine.git',
+    #     'git+https://github.com/MongoEngine/django-mongoengine.git',
+    #     'git+https://github.com/gianburga/culqi-python.git',
+    # ]
+)
